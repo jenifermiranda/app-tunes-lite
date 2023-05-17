07c3ab648d92b9735e8b4b7fa94ed16689e2d14c
Jenifer <jnf.asm@gmail.com>
2023-05-17 13:27:26 +0100

    02.Criar o formulario de identificacao

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,68 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends React.Component {
+  state = {
+    nameLogin: '',
+    loading: false,
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleClick = async () => {
+    const { name } = this.state;
+    const { history } = this.props;
+
+    this.setState({ loading: true });
+    await createUser(name);
+    this.setState({ loading: false });
+    history.push('/search');
+  };
+
   render() {
+    const { nameLogin, loading } = this.state;
+    const n3 = 3;
+    const isDisabled = nameLogin.length < n3;
     return (
       <div data-testid="page-login">
-        Login
+        { loading ? <Loading /> : (
+          <form>
+            Login
+            <input
+              type="text"
+              name="nameLogin"
+              value={ nameLogin }
+              onChange={ this.handleChange }
+              data-testid="login-name-input"
+              placeholder="Nome"
+            />
+            <button
+              type="button"
+              name="login-btn"
+              data-testid="login-submit-button"
+              disabled={ isDisabled }
+              onClick={ this.handleClick }
+            >
+              Entrar
+            </button>
+          </form>
+        )}
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;
