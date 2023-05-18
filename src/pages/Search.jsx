43e53a66f8a076524d324515a9fc43d9c09b3e8b
Jenifer <jnf.asm@gmail.com>
2023-05-18 15:27:13 +0100

    05.Criar o formulario de pesquisa de artista

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,42 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    searchText: '',
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { searchText } = this.state;
+    const n2 = 2;
+    const isDisabled = searchText.length < n2;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            type="text"
+            name="searchText"
+            value={ searchText }
+            onChange={ this.handleChange }
+            data-testid="search-artist-input"
+            placeholder="Nome do Artista"
+          />
+          <button
+            type="submit"
+            name="searchBtn"
+            data-testid="search-artist-button"
+            disabled={ isDisabled }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
